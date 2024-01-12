1aa2f09fc75cd065bad618f00da75ec2353bd610
unknown <m.r.fouladi7@gmail.com>
2024-01-12 14:48:12 +0330

    swagger initilized

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Prisma Example')
+    .setDescription('Nest Prisma Example API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
